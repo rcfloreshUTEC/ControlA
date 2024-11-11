c0ead5d6d3d2df800ac7ad91b2143657f7624461
Roberto Flores <2530892009@mail.utec.edu.sv>
2024-11-10 20:29:24 -0600

    Allow new hosts

--- ControlA/settings.py
+++ ControlA/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-8*dp$^vvb^@&@wrqc_kb1-5$nay&d*9oo!0io_=04i4@q@#kdq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['192.168.68.107', 'localhost', '127.0.0.1']
 
 # Application definition
 
